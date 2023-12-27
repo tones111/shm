d7bf0cfd4ec98a17d66af9b562b3bea31506a85b
Paul Sbarra <sbarra.paul@gmail.com>
2023-12-26 22:02:35 -0600

    add attribution statement

--- src/futex.rs
+++ src/futex.rs
@@ -1,3 +1,6 @@
+// This code derives from Rust Atomics and Locks by Mara Bos (O’Reilly).
+// Copyright 2023 Mara Bos, 978-1-098-11944-7."
+
 use core::sync::atomic::AtomicU32;
 
 // Futex documentation reference:
